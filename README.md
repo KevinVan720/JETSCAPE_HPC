58a45d92330b186b8942d361f40d946ea57daba9
KevinVan720 <kevin950720@gmail.com>
2022-08-09 20:00:25 -0400

    update README

--- README.md
+++ README.md
@@ -1,23 +1,30 @@
 This repo contains python scripts that submits multiple jobs of JETSCAPE on a HPC. Specifically, this repo is for heavy flavor study using the branch:
-https://github.com/JETSCAPE/JETSCAPE-COMP/tree/ReleaseAApaper_for_HD_HF_photon. 
+https://github.com/JETSCAPE/JETSCAPE-COMP/tree/ReleaseAApaper_for_HD_HF_photon. This branch is is based on JETSCAPE 3.1, has the virtuality dependent qhat implementation, and some custom modifications. These modifications include custom event header in the final hadron files that include hydro information for that event (so we can analyze anisotropic flow), modifications to the ColorlessHadronization module so it can read in custom Pythia hadronization settings from the user xml file, and a custom energy loss module that reads in final parton list and feed it directly into hadronization. 
+
+These custom modifications has not been merged into the main branch for the following reasons:
+1. The custom event header is not necessary. You can read in the hydro information directly from the hydro files.
+2. The modifications to the ColorlessHadronization module and the custom energy loss module are there so we can run different hadronization modes with the same final parton file without running the entire event multiple times. I don't think others in JETSCAPE have this kind of need now, and they may come up with a more integrated solution when they do. Please ask in the collaboration if you decide to merge this feature.
 
 ### WSU
 
-The WSU folder contains scripts that were tested on the WSU grid that uses the slurm job scheduler. It is for exploring JETSCAPE with different settings once at a time. You need to put the JETSCAPE folder, the user xml file and the sub_to_run.py file under the same folder. Then type "python3 sub_to_run.py" will submit the jobs that has different pThat bins. 
+The WSU folder contains scripts that were tested on the WSU grid that uses the slurm job scheduler. It is for exploring JETSCAPE with different settings once at a time. You need to put the JETSCAPE folder, the user xml file and the sub_to_run.py script under the same folder. Then type "python3 sub_to_run.py" will submit the jobs with different pThat bins. 
 
-The sub_to_hadronize.py will submit jobs that takes final partons generated from the previous step and hadronize them with the settings you assign. 
+The sub_to_hadronize.py will submit jobs that takes final partons generated from the previous step and hadronize them with the hadronization settings you assign. 
+
+We divide the simulation into two parts so we can study the effects of different hadronization and (maybe) hadronic rescattering modules. 
+
 
 ### NERSC_Bayesian
 
-This folder is used for running JETSCAPE simulations with different settings on NERSC. NERSC also uses the slurm scheduler but some details are different. Also NERSC does not allow you to submit many small jobs. So we will instead use whats called task farmer. 
+This folder is used for running JETSCAPE simulations with different parameters (for Bayesian parameter inference purpose) on NERSC. NERSC also uses the slurm scheduler but some details are different. Also NERSC does not allow you to submit many small jobs. So we will instead use whats called task farmer. 
 
 The steps are the following:
 1. Download the JETSCAPE repo to the same folder. Get all the external data tables.
 2. Run design_jetscape.py to generate the design points.
-3. Run get_jetscape.py to copy JETSCAPE and user xml files to the design point folders.
-4. Run copy_and_generate.py to generate the job scripts for each design point.
+3. Run get_jetscape.py to copy JETSCAPE and user xml files to all the design point folders.
+4. Run copy_and_generate.py to generate the job bash scripts for each design point.
 5. Run generate_tasks.py to generate the final submission scripts.
 
 I choose to generate 5 submission scripts, each contains hundreds of JETSCAPE jobs. Then you need to manually submit these 5 scripts.
-Hadronization is not explored at the moment, as they are found to be not affecting the R_AA's too much. In the future, when we look at more observables, we should use different hadronization settings.
+Hadronization is not explored at the moment, as they are found to be not affecting the R_AA's too much. In the future, when we look at more observables, we may need to use different hadronization settings.
 
--- WSU/sub_to_run.py
+++ WSU/sub_to_run.py
@@ -12,7 +12,7 @@ import pathlib
 ##### generate and submit jobs for running JETSCAPE (not containerized) on a HPC that uses slurm job scheduler.
 ##### the folder structure should be
 ##### JETSCAPE jetscape_init.xml sub_to_run.py
-##### submitting the jobs by running python3 suyb_to_run.py
+##### submitting the jobs by running python3 sub_to_run.py
 
 def checkAndBuildDir(checkDir):
     if (not os.path.isdir(checkDir)):
@@ -52,111 +52,114 @@ checkAndBuildDir(os.path.join(baseDir, "OutputFiles"))
 ##### End of directory checking/creation #####
 
 for n in range(len(pThat_Min)):
+    for m in repeatRange:
 
-    FileStr = str(pThat_Min[n]).zfill(4)+"_"+str(pThat_Max[n]).zfill(4) + \
-            "_ev"
-    OutDir = "OutputFiles/"
+        FileStr = str(pThat_Min[n]).zfill(4)+"_"+str(pThat_Max[n]).zfill(4) + \
+            "_ev_"+str(10*(m)).zfill(3)+"k_to_"+str(10*(m+1)).zfill(3)+"k"
+        OutDir = "OutputFiles/"
 
-    # creating the JETSCPAE user xml file with the appropriate pThatBins. 
-    inputfileName = baseDir+"/InputFiles/jetscape_initBin" + \
-            str(pThat_Min[n])+"_"+str(pThat_Max[n])+".xml"
-    os.system("cp "+baseDir+"/jetscape_init.xml " + inputfileName)
-    for line in fileinput.input([inputfileName], inplace=True):
-        if 'pTHatMin' in line:
-            line = '      <pTHatMin>'+str(pThat_Min[n])+'</pTHatMin>\n'
+        ## create a new user xml file with the appropriate pThat bins and input/output file names
+        inputfileName = baseDir+"/InputFiles/jetscape_initBin" + \
+            str(pThat_Min[n])+"_"+str(pThat_Max[n])+"_"+str(m)+".xml"
+        os.system("cp "+baseDir+"/jetscape_init.xml " + inputfileName)
+        for line in fileinput.input([inputfileName], inplace=True):
+            if 'pTHatMin' in line:
+                line = '      <pTHatMin>'+str(pThat_Min[n])+'</pTHatMin>\n'
 
-        if 'pTHatMax' in line:
-            line = '      <pTHatMax>'+str(pThat_Max[n])+'</pTHatMax>\n'
+            if 'pTHatMax' in line:
+                line = '      <pTHatMax>'+str(pThat_Max[n])+'</pTHatMax>\n'
 
-        if 'outputFilename' in line:
-            line = '<outputFilename>'+baseDir+"/"+OutDir+FileStr+'</outputFilename>\n'
-        sys.stdout.write(line)
+            if 'outputFilename' in line:
+                line = '<outputFilename>'+baseDir+"/"+OutDir+FileStr+'</outputFilename>\n'
+            sys.stdout.write(line)
 
-    Out1 = OutDir+FileStr+".dat"
+        Out1 = OutDir+FileStr+".dat"
 
-    # creating the build directory for each job
-    buildDir = "builds/build_"+FileStr
-    buildDir = os.path.join(baseDir, buildDir)
-    checkAndBuildDir(buildDir)
+        buildDir = "builds/build_"+FileStr
+        buildDir = os.path.join(baseDir, buildDir)
 
-    subFileName = "SubScripts/sub_"+FileStr
-    Input = "InputFiles/jetscape_initBin" + \
-        str(pThat_Min[n])+"_"+str(pThat_Max[n])+".xml"
-       
+        subFileName = "SubScripts/sub_"+FileStr
+        Input = "InputFiles/jetscape_initBin" + \
+            str(pThat_Min[n])+"_"+str(pThat_Max[n])+"_"+str(m)+".xml"
 
-    Input_Par = Out1
-    Output_Par = OutDir+extPartType1+"_"+FileStr+".dat"
+        Input_Par = Out1
+        Output_Par = OutDir+extPartType1+"_"+FileStr+".dat"
 
-    Input_Had = Out1
-    Output_Had = OutDir+extPartType2+"_"+FileStr+".dat"
+        Input_Had = Out1
+        Output_Had = OutDir+extPartType2+"_"+FileStr+".dat"
+ 
+        checkAndBuildDir(buildDir)
 
-    if (os.path.exists(Output_Had)):
-        continue
+        ##### start writing the actual submission script
+        subFile = open(subFileName, "w")
 
-    # writing the actual submission script
-    subFile = open(subFileName, "w")
+        #Write: Header
+        subFile.writelines("#!/usr/bin/env bash\n")
+        subFile.writelines("#SBATCH -q "+queueType+"\n")
 
-    #Write: Header
-    subFile.writelines("#!/usr/bin/env bash\n")
-    subFile.writelines("#SBATCH -q "+queueType+"\n")
+        subFile.writelines("#SBATCH --constraint=intel\n")
+        subFile.writelines("#SBATCH --mem=4GB\n")
+        subFile.writelines("#SBATCH -N 1\n")
+        subFile.writelines("#SBATCH -n 1\n")
 
-    # a single JETSCAPE job takes 1 core, 4GB of RAM 
-    subFile.writelines("#SBATCH --mem=4GB\n")
-    subFile.writelines("#SBATCH -N 1\n")
-    subFile.writelines("#SBATCH -n 1\n")
-
-    subFile.writelines("#SBATCH --job-name=MA_" + str(pThat_Min[n]).zfill(4)+"_"+str(pThat_Max[n]).zfill(4)+"\n")
-    subFile.writelines("#SBATCH -e "+OutDir+"MA_"+FileStr+".err\n")
-    subFile.writelines("#SBATCH -o "+OutDir+"MA_"+FileStr+".log\n")
-    subFile.writelines("#SBATCH --time "+str(maxTime)+":00:00\n")
+        subFile.writelines("#SBATCH --job-name=MA_" +
+                           str(pThat_Min[n]).zfill(4)+"_"+str(pThat_Max[n]).zfill(4)+"\n")
+        subFile.writelines("#SBATCH -e "+OutDir+"MA_"+FileStr+".err\n")
+        subFile.writelines("#SBATCH -o "+OutDir+"MA_"+FileStr+".log\n")
+        subFile.writelines("#SBATCH --time "+str(maxTime)+":00:00\n")
 
         
-    subFile.writelines("mkdir " + buildDir + "/build" + "\n")
-    subFile.writelines("cd " + baseDir + "\n")
-    subFile.writelines("cp -rf JETSCAPE/* " +
+        subFile.writelines("mkdir " +
+                           buildDir + "/build" + "\n")
+        subFile.writelines("cd " + baseDir + "\n")
+        subFile.writelines("cp -rf JETSCAPE/* " +
                             buildDir + "\n")
 
-    # Write: Go to directory
+        # Write: Go to directory
+        subFile.writelines("module unload root\n")
+        subFile.writelines("cd " + buildDir + "/build" "\n")
+        subFile.writelines("cmake ..\n")
+        subFile.writelines("make\n")
 
-    subFile.writelines("cd " + buildDir + "/build" "\n")
-    subFile.writelines("cmake ..\n")
-    subFile.writelines("make\n")
+        #Write: Run
 
-    #Write: Run
-
-    subFile.writelines("srun ./"+preCompiled_Raw+" ../../../"+Input +
-                           " 1> ../../../"+OutDir+"Non-PBS_"+FileStr+"_${SLURM_ARRAY_TASK_ID}.log 2> ../../../"+OutDir+"Non-PBS_"+FileStr+".err\n")
+        subFile.writelines("time ./"+preCompiled_Raw+" ../../../"+Input +
+                           " 1> ../../../"+OutDir+"Non-PBS_"+FileStr+".log 2> ../../../"+OutDir+"Non-PBS_"+FileStr+".err\n")
         
-    subFile.writelines("srun ./"+preCompiled_Par+" ../../../"+Input_Par+" ../../../"+Output_Par+" 1> ../../../" +
+        subFile.writelines("time ./"+preCompiled_Par+" ../../../"+Input_Par+" ../../../"+Output_Par+" 1> ../../../" +
                            OutDir+"Non-PBS_Partons_"+FileStr+".log 2> ../../../"+OutDir+"Non-PBS_Partons_"+FileStr+".err\n")
-
-    subFile.writelines("srun ./"+preCompiled_Had+" ../../../"+Input_Had+" ../../../"+Output_Had+" 1> ../../../" +
+        
+        #now we dont save the hadrons at this step
+        subFile.writelines("time ./"+preCompiled_Had+" ../../../"+Input_Had+" ../../../"+Output_Had+" 1> ../../../" +
                            OutDir+"Non-PBS_Hadrons_"+FileStr+".log 2> ../../../"+OutDir+"Non-PBS_Hadrons_"+FileStr+".err\n")
 
-    #we store the event information in a separate file for future analysis
-    subFile.writelines("cat "+baseDir+"/OutputFiles/"+extPartType2+"_"+FileStr+".dat | grep \"#\" > "+
+        #we store the event information in a separate file for future analysis
+        subFile.writelines("cat "+baseDir+"/OutputFiles/"+extPartType2+"_"+FileStr+".dat | grep \"#\" > "+
             baseDir+"/OutputFiles/"+"Headers"+"_"+FileStr+".dat"+"\n")
-
-    subFile.writelines(
+        
+        subFile.writelines(
             "rm -rf "+buildDir+"\n")
-    # removing the final hadron file to save space 
-    # don't need to do it if you have enough space or the right hadronization settings
-    subFile.writelines(
-            "rm "+baseDir+"/"+Out1+"\n")
 
-    # Write: change mode
-    subFile.writelines("chmod -R g+r ../../../"+OutDir+"\n")
+        # you may remove the final hadron to save space
+        # The hadrons may be recreated as long as we store the final partons
+        #subFile.writelines(
+        #    "rm "+baseDir+"/"+Out1+"\n")
 
-    # Write: create file 'completed' confirming that the job completed
-    subFile.writelines("echo 'Completed!'>../../../" +
+        # Write: change mode
+        subFile.writelines("chmod -R g+r ../../../"+OutDir+"\n")
+
+        # Write: create file 'completed' confirming that the job completed
+        subFile.writelines("echo 'Completed!'>../../../" +
                            OutDir+"completed"+FileStr+"\n")
 
-    # Close submission script file
-    subFile.close()
+        # Close submission script file
+        subFile.close()
 
-    # Now, submit file
-    os.system("cd " + baseDir + " && sbatch " +
+        # Now, submit file
+        os.system("cd " + baseDir + " && sbatch " +
                   subFileName + " > " + subFileName + ".out")
 
-    # ...and wait a few seconds, so as not to overwhelm the scheduler
-    time.sleep(0.5)
+        print("submitted job "+subFileName)
+        # ...and wait a few seconds, so as not to overwhelm the scheduler
+        time.sleep(0.7)
+
